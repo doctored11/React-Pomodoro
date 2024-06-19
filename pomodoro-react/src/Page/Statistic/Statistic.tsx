832f21352601a84b737782c7a9c8b85108fa075e
doctored11 <edit.shultz@yandex.ru>
2024-06-19 21:35:25 +0300

    styles for select

--- pomodoro-react/src/Page/Statistic/Statistic.tsx
+++ pomodoro-react/src/Page/Statistic/Statistic.tsx
@@ -1,12 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./statistic.module.css";
+
+import { Graphic } from "./Graphic/Graphic";
+
+import Choices from "choices.js";
+import "choices.js/public/assets/styles/choices.min.css";
+
 import "../../index.css";
 import "../../media.css";
-import { Graphic } from "./Graphic/Graphic";
+import "./selectStyles.css";
 
 export function Statistic() {
   const [selectedPeriod, setSelectedPeriod] = useState<string>("7");
-
+  const selectRef = useRef<HTMLSelectElement>(null);
   const [dailyStats, setDailyStats] = useState({
     pomodors: 0,
     idleTime: 0,
@@ -37,6 +43,20 @@ export function Statistic() {
     };
 
     fetchDailyStats();
+
+    if (selectRef.current) {
+      const choices = new Choices(selectRef.current, {
+        searchEnabled: false,
+        allowHTML: true,
+        searchChoices: true,
+        placeholder: true,
+        itemSelectText: "",
+      });
+
+      return () => {
+        choices.destroy();
+      };
+    }
   }, []);
 
   const handlePeriodChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -54,7 +74,14 @@ export function Statistic() {
       <div className={styles.statisticsHeader}>
         <h2 className={styles.headerTitle}>Ваша активность</h2>
         <div className={styles.weekSelector}>
-          <select className={styles.select} value={selectedPeriod} onChange={handlePeriodChange}>
+          <select
+            className={`${styles.select} select`}
+            value={selectedPeriod}
+            onChange={handlePeriodChange}
+            ref={selectRef}
+            name="select"
+            id="select"
+          >
             <option value="7">Эта неделя</option>
             <option value="14">Две недели</option>
             <option value="all-time">Все время</option>
@@ -80,17 +107,20 @@ export function Statistic() {
           <Graphic selectedPeriod={selectedPeriod} />
         </div>
 
-        <div className={styles.focusSummary}>
+        <div className={`${styles.summaryBlock}   ${styles.focusSummary}`}>
           <h4 className={styles.summaryTitle}>Фокус</h4>
           <p className={styles.summaryValue}>{dailyStats.focus.toFixed(2)}%</p>
+          <div className={`${styles.iconBlock} ${styles.iconBlock1}`}></div>
         </div>
-        <div className={styles.pauseSummary}>
+        <div className={`${styles.summaryBlock}   ${styles.pauseSummary}`}>
           <h4 className={styles.summaryTitle}>Время на паузе</h4>
           <p className={styles.summaryValue}>{dailyStats.idleTime} сек</p>
+          <div className={`${styles.iconBlock} ${styles.iconBlock2}`}></div>
         </div>
-        <div className={styles.stopsSummary}>
+        <div className={`${styles.summaryBlock}   ${styles.stopsSummary}`}>
           <h4 className={styles.summaryTitle}>Остановки</h4>
           <p className={styles.summaryValue}>{dailyStats.pauses}</p>
+          <div className={`${styles.iconBlock} ${styles.iconBlock3}`}></div>
         </div>
       </div>
     </div>
